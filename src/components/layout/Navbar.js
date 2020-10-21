e41e0781c69f1c4f10748f45e4a45bc3c5a303be
George Flores <georgeflores886@gmail.com>
2020-10-21 16:04:52 -0400

    adjusted and added image in navbar

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,10 +1,12 @@
 import React from 'react'
-
+import Materia from '../../img/materia.png'
 const Navbar = () => {
     return (
         <div className='ff7'>
             <nav className="navbar ff7">
-                <a className="navbar-brand" href="#">Navbar</a>
+                <a className="navbar-brand" href="#" style={{ margin: '0', padding: '0' }}>
+                    <img src={Materia} alt='logo' style={{ width: '30%', padding: '0', margin: 'auto' }} />
+                </a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
