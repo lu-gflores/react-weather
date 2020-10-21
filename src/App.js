e792dc5056441cd37551a71f381fcc3736e8f84d
George Flores <georgeflores886@gmail.com>
2020-10-21 15:50:12 -0400

    created jumbotron and css style

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from './components/layout/Navbar'
+import Jumbotron from './components/layout/Jumbotron'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Jumbotron />
     </div>
   );
 }
