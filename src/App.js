254db3762901d04722f92d9518cc9830165c833d
George Flores <georgeflores886@gmail.com>
2020-10-22 10:55:43 -0400

    modified class to classnames and created container

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Navbar from './components/layout/Navbar'
 import Jumbotron from './components/layout/Jumbotron'
 import './App.css';
 
+console.log(process.env.REACT_APP_OPENWEATHER_API_KEY)
+
 function App() {
   return (
     <div className="App">
