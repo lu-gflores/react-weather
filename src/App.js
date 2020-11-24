f6ca049768fd120020e7537b85084bd09f29cf3b
George Flores <georgeflores886@gmail.com>
2020-11-24 13:01:47 -0500

    removed files and updated class to function for hooks

--- src/App.js
+++ src/App.js
@@ -1,31 +1,19 @@
 import React, { useState, useEffect } from 'react';
+import axios from  'axios'
 import Navbar from './components/layout/Navbar'
 import Jumbotron from './components/layout/Jumbotron'
 import Container from './components/layout/Container'
-import { fetchWeatherLocation } from './api'
 
 import './App.css';
 
+function App() {
 
-class App extends React.Component {
-  state = {
-    data: {},
-    location: ''
-  }
-
-  async componentDidMount() {
-    const fetchedLocation = await fetchWeatherLocation();
-    this.setState({ data: fetchedLocation })
-  }
-
-  handleLocationChange = async (location) => {
-    const fetchedLocation = await fetchWeatherLocation()
-    this.setState({ data: fetchedLocation, location: location })
-  }
-
-  render() {
-    const { data, location } = this.state;
-
+ useEffect(()=> {
+   axios.get(`http://api.openweathermap.org/data/2.5/forecast?q=london&appid=${process.env.REACT_APP_OPENWEATHER_API_KEY}`)
+   .then(data => {
+      console.log(data)
+ }).catch(err => console.log(err)) 
+ }, [])
     return (
       <div className="App">
         <Navbar />
@@ -34,7 +22,7 @@ class App extends React.Component {
 
       </div>
     );
-  }
+
 
 }
 
