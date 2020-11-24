dfa2c23dd49c7d8f0a7a89c4d4663575e8c61483
George Flores <georgeflores886@gmail.com>
2020-11-24 13:59:29 -0500

    updated nav component

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,19 +1,42 @@
 import React from 'react'
+import styled from 'styled-components'
+import {motion} from 'framer-motion'
 import Materia from '../../img/materia.png'
+
 const Navbar = () => {
     return (
-        <div className='ff7'>
-            <nav className="navbar ff7">
-                <a className="navbar-brand" href="#" style={{ margin: '0', padding: '0' }}>
-                    <img src={Materia} alt='logo' style={{ width: '30%', padding: '0', margin: 'auto' }} />
-                </a>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-
-            </nav>
-        </div>
+            <StyledNav>
+                <StyledLogo>
+                    <img src={Materia} alt="Logo"/>
+                    <h1>Weather App</h1>
+                </StyledLogo>
+                <form className='searchCity'>
+                <div class="input-group mb-3">
+                <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default"/>
+                <button type='button' className='btn btn-primary'>Search</button>
+            </div>
+                </form>
+            </StyledNav>
     )
 }
 
+const StyledNav = styled(motion.nav)`
+    padding: 3rem 5rem;
+    text-align: center;
+    input{
+        margin-top: 1.5rem;
+        font-size: 1.5rem;
+        padding: 0.5rem;
+    }
+`
+const StyledLogo = styled(motion.div)`
+    display: flex;
+    justify-content: center;
+    padding: 1rem;
+    cursor: pointer;
+    img{
+        height: 3rem;
+        width: 3rem;
+    }
+`
 export default Navbar
