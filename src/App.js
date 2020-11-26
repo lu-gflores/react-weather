f5cfca7c23d3089b61c48423a2ac515e43776baa
George Flores <georgeflores886@gmail.com>
2020-11-25 21:03:45 -0500

    moved axios and useeffect to weather.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import axios from  'axios'
 import Navbar from './components/layout/Navbar'
 import GlobalStyles from './components/GlobalStyle'
 import Weather from './components/Weather'
 function App() {
-
-  const [weather, setWeather] = useState({})
-
- useEffect(()=> {
-   axios.get(`http://api.openweathermap.org/data/2.5/forecast?q=london&appid=${process.env.REACT_APP_OPENWEATHER_API_KEY}`)
-   .then(data => {
-      console.log(data)
- }).catch(err => console.log(err)) 
- }, [])
 
     return (
       <div className="App container">
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,17 +1,35 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
 
 const Weather = () => {
+
+  const [weather, setWeather] = useState(null)
+
+  useEffect(()=> {
+    axios.get(`http://api.openweathermap.org/data/2.5/forecast?q=london&appid=${process.env.REACT_APP_OPENWEATHER_API_KEY}`)
+    .then(data => {
+        setWeather(data.data)
+         console.log(data.data)
+  }).catch(err => console.log(err)) 
+  }, [])
+ 
     return (
         <>
-        <div className="card" style={{width: '18rem'}}>
-                <img src="..." className="card-img-top" alt="..."/>
+        {weather && (
+        <div>
+            <h1>{weather.city.name}</h1>
+            <div className="card" style={{width: '18rem'}}>
+                <img src={weather.list[2].weather.icon} className="card-img-top" alt="..."/>
              <div className="card-body">
                 <h5 className="card-title">Card title</h5>
             <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
             <a href="#" className="btn btn-primary">Go somewhere</a>
             </div>
         </div>
+        </div>
+        )}
         </>
+        
     )
 }
 
