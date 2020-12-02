70dd57a17dfaeaa03b7d03b777d2c7c9512143f4
George Flores <georgeflores886@gmail.com>
2020-12-02 11:08:57 -0500

    created state for input and render current weather card

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -17,8 +17,22 @@ const Weather = () => {
     <>
         {weather && (
             <div>
-            <h1>{weather.location.name}</h1>
-            <h2>{weather.location.region}</h2>
+
+                <h1>{weather.location.name}</h1>
+                <h2>{weather.location.region}</h2>
+                <h3>{weather.location.country}</h3>
+            {/* Rendering Current weather*/}  
+
+            <div className="card" style={{width: '18rem'}}>
+                <img src={weather.current.condition.icon} className="card-img-top" alt="..."/>
+                <div className="card-body">
+                    <h5 className="card-title">{weather.current.condition.text}</h5>
+                    <p className="card-text">Temperature: {weather.current.temp_c} Celseius</p> 
+                    <p className="card-text">Feels like: {weather.current.feelslike_c} Celseius</p> 
+                    <p className="card-text">Wind Speed: {weather.current.wind_mph}mph</p> 
+                    <p className="card-text">Humidity: {weather.current.humidity}</p> 
+                </div>
+        </div>
             </div>
         )}
              
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React,{useState, useEffect} from 'react'
+import axios from 'axios'
 import styled from 'styled-components'
 import {motion} from 'framer-motion'
 import Materia from '../../img/materia.png'
 
 const Navbar = () => {
+    const [input, setInput] = useState('')
+
+    const weatherInput = e => {
+        setInput(e.target.value)
+    }
+    const searchWeather = () => {
+        axios.get(`http://api.weatherapi.com/v1/forecast.json?key=${process.env.REACT_APP_OPENWEATHER_API_KEY}&q=${input}`)
+        .then(data => {
+            console.log(data)
+        })
+    }
+
     return (
             <StyledNav>
                 <StyledLogo>
@@ -12,8 +25,8 @@ const Navbar = () => {
                 </StyledLogo>
                 <form className='searchCity'>
                 <div className="input-group mb-3">
-                <input type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default"/>
-                <button type='button' className='btn btn-primary'>Search</button>
+                <input onChange={weatherInput} type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default"/>
+                <button onClick={searchWeather} type='button' className='btn btn-primary'>Search</button>
             </div>
                 </form>
             </StyledNav>
